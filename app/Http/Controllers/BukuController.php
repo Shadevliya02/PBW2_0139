8669ffa105dd9d797f8aa6e0cc5ac2f0a5ba6004
Shadevliya02 <devrinasharla.com>
2024-10-17 22:09:41 +0700

    Modul 4

---
+++ app/Http/Controllers/BukuController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Buku;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\StoreBukuRequest;
+use App\Http\Requests\UpdateBukuRequest;
+use Illuminate\Support\Facades\Storage;
+
+class BukuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('buku/index', [
+            'bukus' => DB::table('bukus')->get()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('buku/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBukuRequest $request)
+    {
+        $validatedData = $request->validate([
+            'judul' => 'required',
+            'penulis' => 'required',
+            'kategori' => 'required',
+            'sampul' => 'required|image|file|max:2048',
+        ]);
+
+        if ($request->file('sampul')) {
+            $validatedData['sampul'] = $request->file('sampul')->store('/sampul-buku');
+        }
+
+        Buku::create($validatedData);
+        return redirect('/buku')->with('success', 'Buku berhasil ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Buku $buku)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        $buku = DB::table('bukus')->where('id', $id)->first();
+        return view('buku/update', [
+            'buku' => $buku
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBukuRequest $request, $id)
+    {
+        $validatedData = $request->validate([
+            'judul' => 'required',
+            'penulis' => 'required',
+            'kategori' => 'required',
+            'sampul' => 'image|file|max:2048',
+        ]);
+
+        if ($request->file('sampul')) {
+            if ($request->sampulLama) {
+                Storage::delete($request->sampulLama);
+            }
+            $validatedData['sampul'] = $request->file('sampul')->store('/sampul-buku');
+        }
+
+        Buku::where('id', $id)->update($validatedData);
+        return redirect('/buku')->with('success', 'Buku berhasil diperbarui');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $buku = DB::table('bukus')->select('sampul')->where('id', $id)->first();
+
+        if ($buku && $buku->sampul) {
+            Storage::delete($buku->sampul);
+        }
+
+        Buku::destroy($id);
+        return redirect('/buku')->with('success', 'Buku berhasil dihapus');
+    }
+}
---
+++ resources/views/buku/index.blade.php
@@ -0,0 +1,57 @@
+<x-layout>
+  <div class="container mt-5">
+    <div class="card">
+      <h3 class="card-header">Ini Halaman Buku</h3>
+      <div class="card-body">
+        <div class="col-sm-7 mb-3">
+          <a href="/buku/create" class="btn btn-primary">Tambah Buku</a>
+        </div>
+        <table class="table table-bordered table-striped table-hover" id="myTable">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Judul</th>
+              <th>Penulis</th>
+              <th>Kategori</th>
+              <th>Sampul</th>
+              <th>Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($bukus as $buku)
+            <tr class="text-center">
+              <td class="text-center">{{ $loop->iteration }}</td>
+              <td>{{ $buku->judul }}</td>
+              <td>{{ $buku->penulis }}</td>
+              <td>{{ $buku->kategori }}</td>
+              <td>
+                <img src="{{ asset('storage/' . $buku->sampul) }}" class="rounded img-fluid" width="100px">
+              </td>
+              <td>
+                <a class="btn btn-warning" href="/buku/{{ $buku->id }}/edit">Ubah</a>
+                <form action="/buku/{{ $buku->id }}" method="post" class="d-inline ms-1" onsubmit="return confirm('Apakah Anda yakin ingin menghapus data ini?')">
+                  @method('delete')
+                  @csrf
+                  <button class="btn btn-danger" type="submit">Hapus</button>
+                </form>
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
+  @if(session()->has('success'))
+    <script>
+      Swal.fire({
+        icon: 'success',
+        title: 'Berhasil!',
+        text: '{{ session('success') }}',
+        showConfirmButton: false,
+        timer: 1500
+      });
+    </script>
+  @endif
+</x-layout>
